c7383adaf5d530e1cf0516331a008ae61e50672e
29Shreyas <sihasaneshreyas@gmail.com>
2023-08-01 11:35:14 +0530

    commit

--- level-three/aeon/src/middleware.ts
+++ level-three/aeon/src/middleware.ts
@@ -12,5 +12,5 @@ export async function middleware(req: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/p/:path*/submit', '/p/create'],
+    matcher: ['/p/create'],
 }
